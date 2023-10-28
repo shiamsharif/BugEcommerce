060cac0d20f43a9cf725c6a81298a577bb6e656d
shiamsharif <shiam.sharif.07@gmail.com>
2023-10-28 23:58:38 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./dist/**/*.html"],
   theme: {
-    extend: {},
+    screens:{
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container:{
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily:{
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif"
+      },
+      colors:{
+        'primary' : '#FD3D57'
+      }
+    },
   },
   plugins: [],
 }
